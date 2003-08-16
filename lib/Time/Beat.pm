2347081d4873a73e79fbd12c0c288608987f390d
EMARTIN <emartin@cpan.org>
2003-08-16 13:44:49 +0000

    Import of EMARTIN/Time-Beat-0.2 from CPAN.
    gitpan-cpan-distribution: Time-Beat
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         EMARTIN/Time-Beat-0.2.tar.gz
    gitpan-cpan-author:       EMARTIN
    gitpan-cpan-maturity:     released

--- lib/Time/Beat.pm
+++ lib/Time/Beat.pm
@@ -3,91 +3,84 @@ package Time::Beat;
 use strict;
 use vars qw ( $VERSION @EXPORT_OK );
 
-$VERSION = '1.1';
+$VERSION = '1.2';
 
+use Carp qw( croak );
 use Exporter;
 
 use base qw ( Exporter );
-@EXPORT_OK = qw ( beattime timebeat24 timebeat12 );
+@EXPORT_OK = qw ( beats time24 time12 beattime timebeat24 timebeat12 );
 
-sub beattime {
-	my $time = shift || time();
+# Note that the beattime, timebeat12 and timebeat24 methods are deprecated - see below.
 
-        unless ($time =~ /^\d+$/ && (length($time) == 10)) {
-                return "You can only use the beattime method with a number in the time() format."
-        }
+sub beats {
 
-	my $hsecs;
+    my $time = shift || time();
 
-	my ($sec, $min, $hour, $mday, $mon, $year, $yday, $isdst) =
-		gmtime($time);
+    unless ($time =~ /^\d+$/ and length($time) == 10) {
+        croak "You can only use the 'beats' method with a number in the time() format."
+    }
 
-	if ($hour != 23) {
-  	  $hsecs = (($hour + 1) * 60) * 60;	
-	}
-	$hsecs += ($min * 60);
-	$hsecs += $sec;
-	return sprintf("%i", $hsecs / 86.4);
+    return sprintf "%d", (($time+3600) % 86400) / 86.4;
 }
 
-sub timebeat24 {
-	my $beat = shift;
-	
-	unless ($beat =~ /^\d+$/ && $beat >= 000 && $beat <= 999) {
-		return "You can only use the timebeat24 method with a number in the range 000-999."
-	}
+sub _beats_to_time {
 
-	my $totalsecs = ($beat * 86.4);
-	my $totalmins = sprintf("%i", $totalsecs / 60);
-	my $hours = sprintf("%i", $totalmins / 60);
-	my $mins = ($totalmins % 60);
-	my $secs = (($totalsecs % 3600) % 60);
+    my $beat = shift;
 
-	if (length($mins) == 1) {
-		$mins = "0". $mins;
-	}
+    my $totalsecs = ($beat * 86.4);
+    my $totalmins = int( $totalsecs / 60 );
+    my $hours     = int( $totalmins / 60 );
+    my $mins      = ($totalmins % 60);
+    my $secs      = (($totalsecs % 3600) % 60);
+    $hours--;
+    $hours = 23 if $hours < 0;
+    return ( $hours, $mins, $secs );
+}
 
-	if (length($secs) == 1) {
-		$secs = "0". $secs;
-	}
+sub time24 {
 
-	return "$hours:$mins:$secs";
+    my $beat = shift;
+
+    unless ($beat =~ /^\d+$/ and $beat >= 000 and $beat <= 999) {
+        return "You can only use the 'time24' method with a number in the range 000-999."
+    }
+
+    return sprintf "%02d:%02d:%02d", _beats_to_time( $beat );
+}
+
+sub time12 {
+
+    my $beat = shift;
+
+    unless ($beat =~ /^\d+$/ && $beat >= 000 and $beat <= 999) {
+        return "You can only use the 'time12' method with a number in the range 000-999."
+    }
+
+    my ($hours, $mins, $secs) = _beats_to_time( $beat );
+
+    my $meridiem = ( $hours >= 12 ) ? "pm" : "am";
+    $hours = $hours - 12 if $hours > 12;
+
+    return sprintf "%d:%02d:%02d %s", $hours, $mins, $secs, $meridiem;
+}
+
+# DEPRECATED METHODS 
+# These will be removed in the future.
+
+sub beattime {
+    my $time = shift || time();
+    beats($time);
 }
 
 sub timebeat12 {
-	my $beat = shift;
+    my $beat = shift;
+    time12($beat);
+}
 
-	unless ($beat =~ /^\d+$/ && $beat >= 000 && $beat <= 999) {
-		return "You can only use the timebeat12 method with a number in the range 000-999."
-	}
-
-	my $totalsecs = ($beat * 86.4);
-	my $totalmins = sprintf("%i", $totalsecs / 60);
-	my $hours = sprintf("%i", $totalmins / 60);
-	my $mins = ($totalmins % 60);
-	my $secs = (($totalsecs % 3600) % 60);
-
-	if (length($mins) == 1) {
-		$mins = "0". $mins;
-	}
-
-	if (length($secs) == 1) {
-		$secs = "0". $secs;
-	}
-
-	my $meridiem = "am";
-
-	if ($hours > 12) {
-		$hours = $hours - 12;
-		$meridiem = "pm";
-	}
-
-	# special case for midday
-	if ($hours == 12 && $mins == 0) {
-		$meridiem = "pm";
-	}
-
-	return "$hours:$mins:$secs $meridiem";
+sub timebeat24 {
+    my $beat = shift;
+    time24($beat);
 }
 
 1;
@@ -98,21 +91,21 @@ __END__
 
 Time::Beat - Module to convert between standard time and Swatch ".beat" time.
 
-=head1 SYNOPSIS
-
-use Time::Beat qw(beattime timebeat24 timebeat12);
-
-my $time_in_beats = beattime(time());
-
-my $beats_in_24hr_time = timebeat24($beat);
-
-my $beats_in_12hr_time = timebeat12($beat);
-
 =head1 DESCRIPTION
 
 C<Time::Beat> is a module to convert normal time to and from .beats, of
 which there are a thousand in a day. It can change normal time in time() format 
 to .beats, and .beats into either 24-hour or 12-hour normal time.
+
+=head1 SYNOPSIS
+
+    use Time::Beat qw(beat time24 time12);
+
+    my $time_in_beats = beats($time);
+
+    my $beats_in_24hr_time = time24($beat);
+
+    my $beats_in_12hr_time = time12($beat);
 
 =head1 METHODS
 
@@ -120,29 +113,30 @@ There are three methods in Time::Beat.
 
 =over 4
 
-=item beattime [time string]
+=item * B<beats($time)>
 
-C<beattime> will give you the current time in .beats if you do not specify a
+C<beats> will give you the current time in .beats if you do not specify a
 time string. If you specify a string in C<time()> format it will return that
 particular time in .beats. The basic algorithm for doing this is to take 
 the time in GMT+1 hour, convert it into seconds, and divide by 86.4.
 
-=item timebeat24 [beat string]
+=item * B<time24($beat)>
 
-C<timebeat24> takes a 3-digit beat time and outputs a 24-hour time along the
-lines of "12:34:56".
+C<time24> takes a 3-digit beat time and outputs a 24-hour time along the
+lines of "12:34:56". The hours will have leading noughts.
 
-=item timebeat12 [beat string]
+=item * B<time12($beat)>
 
-C<timebeat12> takes a 3-digit beat time and outputs a 12-hour time along the
-lines of "12:34:56 am" or "12:34:56 pm".
+C<time12> takes a 3-digit beat time and outputs a 12-hour time along the
+lines of "12:34:56 am" or "12:34:56 pm". Hours do not have leading noughts.
 
 =back
 
 =head1 CHANGES
 
-As of 1.1 the module can now convert to and from .beats in 12- and 24-hour
-format.
+I<Important:> As of v1.11 the method names have changed. The former method
+names, C<beattime>, C<timebeat24> and C<timebeat12> will still work, but they
+have been B<deprecated>, and will be removed at some point in the future.
 
 =head1 AUTHOR
 
@@ -151,9 +145,17 @@ Earle Martin <emartin@cpan.org>. Originally written by James Duncan
 
 =head1 SEE ALSO
 
-L<DateTime::Format::IBeat>, which does much the same thing as part of the
-DateTime project, and the .beat home page: 
+=over 4
+
+=item * L<DateTime::Format::IBeat>
+
+This module by Iain Truskett <spoon@cpan.org> does much the same thing but
+in an OO fashion, and is part of the L<DateTime> project; if you need
+something more sophisticated, I would suggest you look into that. My thanks
+also to Iain for contributing patches to this module. 
+
+=item * The .beat home page
+
 L<http://www.swatch.com/itime_tools/itime.php>
 
 =cut
-
--- t/beat.t
+++ t/beat.t
@@ -1,13 +1,20 @@
-use Test;
+#!/usr/bin/perl -w
+use strict;
+use Test::More tests => 8;
 
-BEGIN { plan tests => 3 }
+use Time::Beat qw( beattime timebeat12 timebeat24 );
 
-use Time::Beat qw(beattime timebeat12 timebeat24);
+{
+    is( timebeat24( 0 ) => '23:00:00', 'Beat 0 is 11pm, GMT' );
+    is( timebeat24( 500 ) => '11:00:00', 'Beat 500 is 11am, GMT' );
+    is( beattime( 1060815600 ) => '0', '11pm, GMT is beat 0' );
+    is( beattime( 1060862400 ) => '541', 'noon, GMT is beat 541' );
+}
 
-$a = beattime(1060780770);
-$b = timebeat12(596);
-$c = timebeat24(596);
+{
+    is( timebeat24( 348 ) => '07:21:07', 'Beat 348 is 7.21am, GMT' );
+    is( timebeat12( 348 ) => '7:21:07 am', 'Beat 348 is 7.21am, GMT' );
+    is( timebeat12( 648 ) => '2:33:07 pm', 'Beat 348 is 7.21am, GMT' );
+    is( beattime( 1060845686 ) => '348', '7.21am GMT is 348m' );
+}
 
-ok( $a, '596' ); 		# converts from time() format to beats OK
-ok( $b, '2:18:14 pm' );		# converts from beats to 12hr time OK
-ok( $c, '14:18:14' );		# converts from beats to 24hr time OK
